65ee04c27634fbbf9e143e342ce15cea1ef8e26c
alexizator <akilesh0876@gmail.com>
2021-08-04 22:18:52 +0530

    Intial git

--- CryptOn-master_legal/create_db.py
+++
@@ -1,9 +0,0 @@
-import dbman
-def create():
-    dbman.cursor.execute("CREATE DATABASE IF NOT EXISTS CRYPTON;")
-def profiles():
-    dbman.cursor.execute("CREATE TABLE IF NOT EXISTS PROFILES(USERNAME TEXT, PASSWORD TEXT);")
-def db_select():
-    dbman.cursor.execute("USE CRYPTON;")                    
-def users(username):
-    dbman.cursor.execute(f"CREATE TABLE IF NOT EXISTS {username}(enc_text longtext, enc_key longtext);")
